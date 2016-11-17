22ab47230ae5b8745a14bfb7615cbddf4a94e0ce
erick499 <erick.t.oliveira@gmail.com>
2016-11-17 00:52:09 -0200

    commit 17/11-00:50
    Pathfinding inacabado

---
+++ pathFinding A/Assets/adm.cs
@@ -0,0 +1,176 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class adm : MonoBehaviour {
+
+	public GameObject bloco;
+    public propriedadesBlocos inicial;
+	public propriedadesBlocos final;
+    public static int count = 0;
+    public int columns = 10;
+    public int rows = 10;
+	// Use this for initialization
+	void Start () {
+
+		for (int i = 0; i < columns; i++) {
+			for(int j = 0; j < rows; j++){
+
+				bloco.GetComponent<propriedadesBlocos>().i = i;
+				bloco.GetComponent<propriedadesBlocos>().j = j;
+				Instantiate(bloco,new Vector3(0.15f * i, 0.15f* j, 0f), bloco.transform.rotation);
+
+			}
+		}
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetKeyDown("s"))
+       {
+
+            pathFinding(inicial);
+
+       }
+
+	}
+
+    void pathFinding(propriedadesBlocos blocoCopia)
+    {
+
+        List<propriedadesBlocos> blocoList = new List<propriedadesBlocos>();
+
+        if (blocoCopia.name != final.name && blocoCopia.name != null)
+        {
+            propriedadesBlocos temp;
+            blocoList.Add(blocoCopia);
+            int minVal = 100000;
+
+            for (int i = -1; i < 2; i++)
+            {
+                for (int j = -1; j < 2; j++)
+                {
+                    if (i != 0 || j != 0)
+                    {
+                        temp = Find(blocoCopia, i, j);
+
+                        if (!temp.selected)
+                        {
+                            if (i == 0 || j == 0)
+                            {
+                                if (TotalValue(temp, final, 10) <= minVal)
+                                {
+                                    if (TotalValue(temp, final, 10) < minVal)
+                                    {
+                                        blocoList.Clear();
+                                        minVal = TotalValue(temp, final, 10);
+                                    }
+
+                                    blocoList.Add(temp);
+                                }
+                            }
+                            else if (TotalValue(temp, final, 14) <= minVal)
+                            {
+                                if (TotalValue(temp, final, 14) < minVal)
+                                {
+                                    blocoList.Clear();
+                                    minVal = TotalValue(temp, final, 14);
+                                }
+
+                                blocoList.Add(temp);
+                            }
+
+                        }
+                    }
+                }
+            }
+
+        }
+    }
+
+    public void ReturnPathFinding(propriedadesBlocos actual)
+    {
+        if (actual.name != inicial.name)
+        {
+            int MV = 100000;
+            for (int i = -1; i < 2; i++)
+            {
+                for (int j = -1; j < 2; j++)
+                {
+                    if (i != 0 || j != 0)
+                    {
+                        if (Find(actual, i, j).selected)
+                        {
+                            if (i == 0 || j == 0)
+                            {
+                                if (TotalValue(Find(actual, i, j), inicial, 10) < MV)
+                                    MV = TotalValue(Find(actual, i, j), inicial, 10);
+                            }
+                            else
+                            {
+                                if (TotalValue(Find(actual, i, j), inicial, 14) < MV)
+                                    MV = TotalValue(Find(actual, i, j), inicial, 14);
+                            }
+                        }
+                    }
+                }
+            }
+
+            for (int i = -1; i < 2; i++)
+            {
+                for (int j = -1; j < 2; j++)
+                {
+                    if (i != 0 || j != 0)
+                    {
+                        if (!Find(actual, i, j).type.Equals("inicial"))
+                        {
+                            if (Find(actual, i, j).selected)
+                            {
+                                if (i == 0 || j == 0)
+                                {
+                                    if (TotalValue(Find(actual, i, j), inicial, 10) == MV)
+                                    {
+                                        Find(actual, i, j).verified = true;
+                                        Find(actual, i, j).selected = false;
+                                        ReturnPathFinding(Find(actual, i, j));
+                                    }
+                                }
+                                else
+                                {
+                                    if (TotalValue(Find(actual, i, j), inicial, 14) == MV)
+                                    {
+                                        Find(actual, i, j).verified = true;
+                                        Find(actual, i, j).selected = false;
+                                        ReturnPathFinding(Find(actual, i, j));
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    public int TotalValue(propriedadesBlocos Ot, propriedadesBlocos Tdest, int V)
+    {
+        if (Ot.type == "wall"|| Ot == null)
+        {
+            return 50000;
+       }
+
+        return (V + 10 * (Mathf.Abs(Tdest.i - Ot.i) + Mathf.Abs(Tdest.j - Ot.j)));
+    }
+
+    public propriedadesBlocos Find(propriedadesBlocos blococopia, int i, int j)
+    {
+        if (blococopia.i + i < 0 || blococopia.j + j < 0)
+            return null;
+        else
+            return GameObject.Find((blococopia.i + i).ToString() + (blococopia.j + j).ToString()).GetComponent<propriedadesBlocos>();
+    }
+
+
+}
---
+++ pathFinding A/Assets/propriedadesBlocos.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class propriedadesBlocos : MonoBehaviour {
+
+    public string type = "floor";
+
+	public bool verified, selected = false;
+
+	public int i,j;
+
+	void Start(){
+
+		this.gameObject.name = i+""+j;
+      
+
+	}
+
+    void Update()
+    {
+        if (selected) GetComponent<SpriteRenderer>().color = Color.yellow;
+        if (verified) GetComponent<SpriteRenderer>().color = Color.green;
+    }
+
+
+    void OnMouseDown(){
+
+        if (adm.count == 0)
+        {
+
+            SpriteRenderer renderer = GetComponent<SpriteRenderer>();
+            renderer.color = new Color(0f, 1f, 1f, 1f);
+            type = "inicial";
+            FindObjectOfType<adm>().inicial = this.GetComponent<propriedadesBlocos>();
+
+            adm.count++;
+
+        }
+        else if (adm.count == 1)
+        {
+
+            SpriteRenderer renderer = GetComponent<SpriteRenderer>();
+            renderer.color = new Color(1f, 0f, 0f, 1f);
+            type = "final";
+            FindObjectOfType<adm>().final = this.GetComponent<propriedadesBlocos>();
+            
+
+            adm.count++;
+
+        }
+
+        else if (adm.count == 2)
+        {
+
+            SpriteRenderer renderer = GetComponent<SpriteRenderer>();
+            renderer.color = new Color(0f, 0f, 0f, 1f);
+
+            type = "wall";
+
+        }
+
+    }
+
+}
